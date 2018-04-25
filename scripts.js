11bef10592eab5e47fc814cd01cfc22e96dc34cd
exetertech <support@exetertechnicalsupport.co.uk>
2018-04-25 18:34:50 +0100

    .
    .

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
-}
+  $(".text1").click(function() {
+    $(".col-md-4").slideToggle();
+  });
+  $(".text2").click(function() {
+    $(".col-md-3").slideToggle();
+  });
+});
